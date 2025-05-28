342112fc00ef3e9944d3d62aae4ba6a49c0e9a26
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-05-28 12:48:19 +0200

    EA8 hasta el 4

--- EA8. Agregacions/M2NF4EA8_SaidiAsad.js
+++ EA8. Agregacions/M2NF4EA8_SaidiAsad.js
@@ -1,46 +1,45 @@
 //1) De la col·lecció «books» mostra tots els llibres que tinguin com a mínim 5 authors.
 db.books.aggregate([
+  { $project: {
+      _id: 0,
+      title: 1,
+      authors: { $size: "$authors" }
+    }
+  },
+    { $match: { authors: { $gte: 5 } } }
+]);
+
+//2) De la col·lecció «books» mostra els llibres ordenats per número d’autors de forma descendent. Primer els llibres amb més autors i al final els llibres amb menys autors.
+db.books.aggregate([
     { $project: {
         _id: 0,
         title: 1,
         authors: { $size: "$authors" }
-      }
-    },
-      { $match: { authors: { $gte: 5 } } }
-  ]);
-  
-  //2) De la col·lecció «books» mostra els llibres ordenats per número d’autors de forma descendent. Primer els llibres amb més autors i al final els llibres amb menys autors.
-  db.books.aggregate([
-      { $project: {
-          _id: 0,
-          title: 1,
-          authors: { $size: "$authors" }
-      }
-      },
-      {
-          $sort: {
-              authors: -1
-          }
-      }
-  ]);
-  
-  //3) De la col·lecció «books» mostra els ISBN per cada Status "status". Utilitza l’estructura aggregate, i utilitza les funcions $group i $addToSet.
-  db.books.aggregate([
-    {
-      $group: {
-        _id: "$status",
-        isbns: { $addToSet: "$ISBN" }
-      }
     }
-  ])
-  
-  //4) De la col·lecció «restaurants» mostra quantes valoracions "grades" té cada restaurant.
-  db.books.aggregate([
-    { $project: {
-        _id: 0,
-        title: 1,
-        score: { $size: "$grades.score" }
-      }
     },
-      { $match: { authors: { $gte: 5 } } }
-  ]);
+    {
+        $sort: {
+            authors: -1
+        }
+    }
+]);
+
+//3) De la col·lecció «books» mostra els ISBN per cada Status "status". Utilitza l’estructura aggregate, i utilitza les funcions $group i $addToSet.
+db.books.aggregate([
+  {
+    $group: {
+      _id: "$status",
+      isbns: { $addToSet: "$ISBN" }
+    }
+  }
+])
+
+//4) De la col·lecció «restaurants» mostra quantes valoracions "grades" té cada restaurant.
+db.restaurants.aggregate([
+  {$project:{
+      _id: 0,
+      title: 1,
+      grades: {$size:"$grades"}
+    }
+  }
+])
